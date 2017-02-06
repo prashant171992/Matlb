abb28187b0a44a7fae1a01c0c32f619d789978cd
Prashant <prashant171992@gmail.com>
2017-02-07 00:50:17 +0530

    poller 2.0 , minor fix

--- src/main/java/com/matlb/controller/MatlbController.java
+++ src/main/java/com/matlb/controller/MatlbController.java
@@ -110,7 +110,7 @@ public class MatlbController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/poll/asked/get/{pollId}")
     public BasePollResponse getPollAskedDetailsById(@RequestBody User user, @PathVariable Integer pollId) {
-        return getPollService().getPollDetailsById(user, pollId);
+        return getPollService().getPollAskedDetailsById(user, pollId);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/poll/cat")
